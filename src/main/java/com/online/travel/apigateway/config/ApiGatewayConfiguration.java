577d60e714505463bbad63057cbeea9eb67732ef
Abhisek Bajoria <abhisek.bajoria@publicissapient.com>
2023-06-19 20:33:21 +0530

    Initial commit

--- src/main/java/com/online/travel/apigateway/config/ApiGatewayConfiguration.java
+++ src/main/java/com/online/travel/apigateway/config/ApiGatewayConfiguration.java
@@ -1,25 +1,25 @@
-package com.online.travel.apigateway.config;
-
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApiGatewayConfiguration {
-
-    @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(p -> p
-                        .path("/get")
-                        .filters(f -> f
-                                .addRequestHeader("MyHeader", "MyURI")
-                                .addRequestParameter("Param", "MyValue"))
-                        .uri("http://httpbin.org:80"))
-                .route(p -> p.path("/hotel/**")
-                        .uri("lb://hotel"))
-                .build();
-    }
-
-}
+//package com.online.travel.apigateway.config;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ApiGatewayConfiguration {
+//
+//    @Bean
+//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/get")
+//                        .filters(f -> f
+//                                .addRequestHeader("MyHeader", "MyURI")
+//                                .addRequestParameter("Param", "MyValue"))
+//                        .uri("http://httpbin.org:80"))
+//                .route(p -> p.path("/hotel/**")
+//                        .uri("lb://hotel"))
+//                .build();
+//    }
+//
+//}
